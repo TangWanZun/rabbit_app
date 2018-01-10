328987ae30a1b7673019bc12c29ed1f0af6127d9
TangWanZun <997017913@qq.com>
2018-01-10 10:30:16 +0800

    歌曲可以添加到 了曲库之中

--- js/audio.js
+++ js/audio.js
@@ -1,33 +1,32 @@
-//歌曲搜索
-function searchAudio(audioSearchContent){
-	var jsonp = document.createElement("script");
-	var url = "http://songsearch.kugou.com/song_search_v2?callback=audioListShow&keyword="+audioSearchContent;
-	jsonp.src = url;
-	jsonp.id = "jsonp";
-	document.body.appendChild(jsonp);
-}
-//歌曲搜索回调方法
-function audioListShow(json){
-	//	清空列表
-	AUDIO.audioSearchList.splice(0,AUDIO.audioSearchList.length);
-	console.log(json);
-	//遍历循环添加数据
-	for(let i in json.data.lists){
-//	    createAudioList(json.data.lists[i],i+1);
-	    AUDIO.audioSearchList.push({
-	    	name:json.data.lists[i].SongName,
-	    	geshou:json.data.lists[i].SingerName,
-	    	time:json.data.lists[i].AlbumName
-	    });
+var AudioOpera = {
+	//歌曲搜索
+	searchAudio:function(audioSearchContent){
+		var jsonp = document.createElement("script");
+		var url = "http://songsearch.kugou.com/song_search_v2?callback=AudioOpera.audioListShow&keyword=" + audioSearchContent;
+		jsonp.src = url;
+		jsonp.id = "jsonp";
+		document.body.appendChild(jsonp);
+	},
+	//歌曲搜索回调方法
+	audioListShow:function(json){
+		//	清空列表
+		AUDIO.audioSearchList.splice(0, AUDIO.audioSearchList.length);
+		//遍历循环添加数据
+		for(let i in json.data.lists) {
+			AUDIO.audioSearchList.push({
+				name: json.data.lists[i].SongName,
+				geshou: json.data.lists[i].SingerName,
+				time: json.data.lists[i].AlbumName
+			});
+		}
+		document.body.removeChild(window.parent.document.getElementById("jsonp"));
+	},
+	//歌曲添加
+	audioAdd:function(index){
+		AUDIO.menuList.push(AUDIO.audioSearchList[index]);
+		mui.toast("歌曲添加成功", {
+			duration: 'short',
+			type: 'div'
+		});
 	}
-	document.body.removeChild(window.parent.document.getElementById("jsonp"));
 }
-function createAudioList(audioListViow,n){
-//	AUDIO.$data.audios.push({name:audioListViow.SongName,geshou:audioListViow.SingerName,time:audioListViow.AlbumName});
-}
-//歌曲添加
-function audioAdd(n){
-	AUDIO_ARR.$data.audios.push(AUDIO_LIST.$data.audios[n]);
-	mui.toast("歌曲添加成功",{duration:'short', type:'div' });
-}
-
